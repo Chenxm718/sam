9775e9d79fdf6739ef6de765a0787afbc5f97aa6
ChenXinmin <xinmin.chen@mtime.com>
2019-02-26 17:48:55 +0800

    test

--- common-utils/src/test/java/com/sam/test/TestLambda.java
+++ common-utils/src/test/java/com/sam/test/TestLambda.java
@@ -3,6 +3,9 @@ package com.sam.test;
 import com.sam.utils.lambda.FunctionDemo;
 import org.junit.Test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -21,8 +24,25 @@ public class TestLambda {
         list.forEach(li-> System.out.println(li));
     }
 
+    @Test
     public void test1(){
-        int t = 10 ;
-//        FunctionDemo.function1(1,fun->FunctionDemo.function1(fun));
+        int   n = 10 , max = 0 , min = 0 , temp = 0;
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            max = min = Integer.parseInt(br.readLine( ));
+        } catch ( IOException e ) {
+            System.out.println(e.getMessage());
+        }
+        for ( int i = 0 ; i <= n ; i ++ ) {
+            try {
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                temp = Integer.parseInt(br.readLine( ));
+                if (temp > max ) max=temp;
+                if (temp < min) min=temp;
+            } catch ( IOException e ) {
+                System.out.println(e.getMessage());
+            }
+        }
+        System.out.println("max number = "+max+", min number = "+min);
     }
 }
--- common-utils/src/test/java/com/sam/test/TestSubList.java
+++ common-utils/src/test/java/com/sam/test/TestSubList.java
@@ -56,4 +56,68 @@ public class TestSubList {
         System.out.println(startDate.toLocalDate().isBefore(LocalDate.parse("2017-09-02")));
         System.out.println("test"+"_"+ startDate.format(DateTimeFormatter.ofPattern("yyyyMM")));
     }
+
+    @Test
+    public void test2(){
+        int x =1;
+        int y=2;
+        int z=3;
+        System.out.println(y+=z--/++x);
+    }
+
+    @Test
+    public void test3(){
+        int a=100;
+        double result = 0.0;
+        for(int i=1;i<=a;i++){
+            result+=1/i;
+        }
+        System.out.println(result);
+    }
+
+//    public class count100(){
+//        public static void main(String[] args) {
+//            int a=100;
+//            double result = 0.0;
+//            for(int i=1;i<=a;i++){
+//                result+=1/i;
+//            }
+//            System.out.println(result);
+//        }
+//    }
+
+    //每行输入一个数值，输入多个数值
+    public  static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int [] a = new int[10];
+        int count = 0;
+        System.out.println("请输入10个整数");
+        while (true) {
+            if(scanner.hasNextInt()){
+                a[count++] = scanner.nextInt();
+            }
+            if(count==10){
+                for (int i = a.length-1; i >= 0; i--) {
+                    System.out.println(a[i]);
+                }
+                break;
+            }
+        }
+    }
+
+
+    @Test
+    public void test4(){
+        int a= 100;
+        int b = 45;
+        System.out.println(max(a,b));
+    }
+
+    public static double max(double a,double b){
+        if (a>b){
+            return a;
+        }else {
+            return b;
+        }
+    }
 }
